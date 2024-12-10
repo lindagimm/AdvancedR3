2148c24eaaed1e94413323011a2283d6d055d6c3
Linda Gimm <lindagimm98@gmail.com>
2024-12-10 16:21:02 +0100

    added descriptive stats to functions

---
+++ R/functions.R
@@ -0,0 +1,18 @@
+#' Function for descriptice statistics
+#'
+#' @param data
+#'
+#' @return A data.frame/Tibble
+
+#' Function for descriptice statistics
+#'
+#' @param data
+#'
+#' @return A data.frame/Tibble
+
+descriptive_stats <- function(data) {
+    data |>
+        dplyr::group_by(metabolite) |>
+        dplyr::summarise(dplyr::across(value, list(mean = mean, sd = sd))) |>
+        dplyr::mutate(dplyr::across(tidyselect::where(is.numeric), ~ round(.x, digits = 1)))
+}
